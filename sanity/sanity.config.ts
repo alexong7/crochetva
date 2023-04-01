b40d23937a55229489fb408fa267c9bd3539550b
agong <agong@iastate.edu>
2023-04-01 03:58:37 -0500

    fix imports

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,7 +1,10 @@
-import {defineConfig} from 'sanity'
+
 import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
+
+import {defineField, defineType} from 'sanity/lib/exports'
+import {defineConfig} from 'sanity/lib/exports'
 
 export default defineConfig({
   name: 'default',
